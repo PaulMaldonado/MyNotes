9061f436503807c2485a70cbaf7357b65546a554
cbta258 <cbta.258@gmail.com>
2018-08-06 22:59:34 -0500

    Added profile of users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :notes
+  resources :usuarios, as: :users, only: [:show, :update]
 
   authenticated :user do
     root 'home#index'
